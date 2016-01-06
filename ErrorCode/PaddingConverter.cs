88f6e2a8ebf8d76909f5c94f400a45d79de7d172
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2016-01-06 19:32:19 +0100

    Fixed width issues on long texts textblocks won't wrap inside a contentpresenter without a width/maxwidth set somewhere

---
+++ ErrorCode/PaddingConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ErrorCode
+{
+    class PaddingConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+        
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int padding = parameter == null || parameter == DependencyProperty.UnsetValue
+                ? -25 
+                : int.Parse((string)parameter);
+
+            var actual = (double)value;
+
+
+            return actual + padding;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
