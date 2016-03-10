5f029eeeedea6fbbc6294cbae134982f380d9048
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2016-03-10 18:21:17 +0100

    Allow regex discovery

--- ErrorCode/Discover.cs
+++ ErrorCode/Discover.cs
@@ -3,14 +3,20 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using ErrorCode.Domain;
+using System.Text.RegularExpressions;
 
 namespace ErrorCode
 {
     static class Discover
     {
-        public static IReadOnlyCollection<TestAssembly> Tests(string assemblyFilter = "*test*.dll")
+        static Regex _assemblyFilter = new Regex(@".*tests?\.(dll|exe)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        public static IReadOnlyCollection<TestAssembly> Tests() => Tests(_assemblyFilter);
+
+        public static IReadOnlyCollection<TestAssembly> Tests(Regex assemblyFilter)
         {
-            var tests = Directory.GetFiles(Directory.GetCurrentDirectory(), assemblyFilter)
+            var tests = Directory.GetFiles(Directory.GetCurrentDirectory())
+                                 .Where(x => assemblyFilter.IsMatch(Path.GetFileName(x)))
                                  .Select(Assembly.LoadFrom)
                                  .Select(x => new TestAssembly(x))
                                  .ToArray();
